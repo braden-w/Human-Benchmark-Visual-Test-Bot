4c60a1293c67acda3c3af4990c91f22c52381ff4
Braden Wong <bmw02002turbo@gmail.com>
2020-07-15 11:47:39 -0700

    Redid memorization step

--- human-benchmark-visual-memory-test-bot.py
+++ human-benchmark-visual-memory-test-bot.py
@@ -1,23 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Firefox()
 driver.get("https://humanbenchmark.com/tests/memory")
 
 
-try:
-    while True:
-        notfound = True
-        while notfound:
-            newboxes = [
-                element
-                for element in driver.find_elements_by_class_name("active")
-            ]
-            notfound = len(newboxes) == 0
-        time.sleep(1)
-        for element in newboxes:
-            try:
-                element.click()
+while True:
+    element = WebDriverWait(driver, 100, poll_frequency=0.001).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".square.active"))
+    )
+    newboxes = [
+        element for element in driver.find_elements_by_css_selector(".square.active")
+    ]
+    # notfound = True
+    # while notfound:
+    #     newboxes = [
+    #         element
+    #         for element in driver.find_elements_by_class_name("active")
+    #     ]
+    #     notfound = len(newboxes) <= 1
+    for element in newboxes:
+        element.click()
             except:
                 continue
         
